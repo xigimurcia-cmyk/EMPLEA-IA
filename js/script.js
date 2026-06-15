1efb9019cc3ef1bdba15665ec755d7440eba6bd3
Xitlaly Murcia <xigimurcia@gmail.com>
2026-06-14 20:00:21 -0600

    Agregue registro-empresas.html y css

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Función para mostrar u ocultar la contraseña
+function mostrarContrasena(id) {
+  const input = document.getElementById(id); // busca el campo por su id
+  if (input.type === "password") {           // si está oculto
+    input.type = "text";                     // lo muestra
+  } else {                                   // si está visible
+    input.type = "password";                 // lo oculta
+  }
+}
